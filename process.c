b5a9a9ae305d5a8341e23bdf971957679acd3691
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-11-05 20:47:04 -0300

    Adiciona funções de processos

---
+++ process.c
@@ -0,0 +1,102 @@
+#include "process.h"
+#include "util.h"
+
+const char* getPriorityAsString(int priority) {
+    switch(priority){
+        case HIGH: return "High";
+        case LOW: return "Low";
+        case IO: return "IO";
+        default : break;
+    }
+}
+
+const char* getStatusAsString(int status) {
+    switch(status) {
+        case NOT_STARTED: return "Not started";
+        case READY: return "Ready";
+        case RUNNING: return "Running";
+        case BLOCKED: return "Blocked";
+        case FINISHED: return "Finished";
+        default : break;
+    }
+}
+
+const char* getIoTypeAsString(int io_type) {
+    switch(io_type){
+        case DISK : return "Disk";
+        case MAGNETIC_TAPE : return "Magnetic Tape";
+        case PRINTER : return "Printer";
+        case NONE : return "None";
+        default : break;
+    }
+
+}
+
+void printProcess(process* p) {
+    printf("=== Process Description ===\n");
+    printf("PID: %d\nDuration: %d\nPPID: %d\nPRIORITY: %s\nSTATUS: %s\nIO TYPE: %s\n\n",
+        p->pid,
+        p->duration,
+        p->ppid,
+        getPriorityAsString(p->priority),
+        getStatusAsString(p->status),
+        getIoTypeAsString(p->ioType));
+}
+
+// generate PID incrementally
+pid_t generateIncrementalPID() {
+    static pid_t lastPID = 0;
+    return ++lastPID;
+}
+
+// Randomness generators
+pid_t generateRandomPID() {
+    return rand() % 1000;
+}
+
+int generateRandomDuration() {
+    return (rand() % MAX_DURATION) + 1;
+}
+
+int generateRandomStartingTime() {
+    return (rand() % MAX_STARTING_TIME) + 1;
+}
+
+int generateRandomPriority() {
+    return rand() % PRIORITY_SIZE;
+}
+
+int generateRandomIO() {
+    return rand() % IO_TYPE_SIZE;
+}
+
+// Process initializers
+process* initRandomProcess() {
+    process* proc = malloc(sizeof(process));
+    proc->duration = generateRandomDuration();
+    proc->startingTime = generateRandomStartingTime();
+    proc->pid = generateIncrementalPID();
+    proc->ppid = 0;     // fix parent pid at 0, TODO: change?
+    proc->priority = generateRandomPriority();
+    proc->status = NOT_STARTED;
+    proc->ioType = generateRandomIO();
+    return proc;
+}
+
+process** initRandomProcesses() {
+    // Allocate processes in the heap
+    process** initializedProcesses = calloc(MAX_PROCESSES, sizeof(process*));
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        initializedProcesses[i] = initRandomProcess();
+        printProcess(initializedProcesses[i]);
+    }
+    return initializedProcesses;
+}
+
+int allProcessFinished(process** processes) {
+    for (int i = 0; i < MAX_PROCESSES; i++) {
+        if (processes[i]->status != FINISHED)
+            return FALSE;
+    }
+    return TRUE;
+}
---
+++ process.h
@@ -0,0 +1,45 @@
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define PRIORITY_SIZE 3
+#define IO_TYPE_SIZE 4
+
+typedef enum {IO, HIGH, LOW} PRIORITY;
+typedef enum {DISK, MAGNETIC_TAPE, PRINTER, NONE} IO_TYPE;
+typedef enum {NOT_STARTED, READY, BLOCKED, RUNNING, FINISHED} STATUS;
+
+typedef struct {
+
+    int duration;
+    int startingTime;
+    int pid;
+    int ppid;
+    int returnedFromIO;
+    int preempted;
+    PRIORITY priority;
+    STATUS status;
+    IO_TYPE ioType;
+
+} process;
+
+const char* getPriorityAsString(int);
+const char* getStatusAsString(int);
+const char* getIoTypeAsString(int);
+void printProcess(process*);
+pid_t generateRandomPID();
+pid_t generateIncrementalPID();
+int generateRandomDuration();
+int generateRandomStartingTime();
+int generateRandomPriority();
+int generateRandomIO();
+process* initRandomProcess();
+process** initRandomProcesses();
+int allProcessFinished(process**);
+
+#endif
