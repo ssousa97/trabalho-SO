5eac49a9583e913b62d092891d6338778398ae72
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-11-05 20:47:59 -0300

    Adiciona definições de constantes

---
+++ util.h
@@ -0,0 +1,12 @@
+// ANSI colors
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_PROCESSES 10
+#define MAX_STARTING_TIME 16
+#define MAX_DURATION 10
