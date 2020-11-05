5aa36d6242c0ececeeb111633da49e97cd75532e
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-11-05 19:59:28 -0300

    Adiciona métodos iniciais de controle de processo

--- trabalho.c
+++ trabalho.c
@@ -4,11 +4,18 @@
 #include <sys/types.h>
 #include <time.h>
 
+// ANSI colors
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 #define TRUE 1
 #define FALSE 0
 
 #define MAX_PROCESSES 10
 #define MAX_STARTING_TIME 16
+#define MAX_DURATION 10
 
 #define PRIORITY_SIZE 3
 #define IO_TYPE_SIZE 4
@@ -16,13 +23,16 @@
 //definições
 typedef enum {IO, HIGH, LOW} PRIORITY;
 typedef enum {DISK, MAGNETIC_TAPE, PRINTER, NONE} IO_TYPE;
-typedef enum {READY, BLOCKED, RUNNING, FINISHED} STATUS;
+typedef enum {NOT_STARTED, READY, BLOCKED, RUNNING, FINISHED} STATUS;
 
 typedef struct {
 
     int duration;
+    int startingTime;
     int pid;
     int ppid;
+    int returnedFromIO;
+    int preempted;
     PRIORITY priority;
     STATUS status;
     IO_TYPE ioType;
@@ -31,12 +41,22 @@ typedef struct {
 
 typedef struct {
 
-    process* p;
-    struct queue* next;
-    struct queue* front;
-    struct queue* back;
+    process** processes;
+    int size;
 
 } queue;
+
+typedef struct {
+
+    int cycles;
+    int quantum;
+    int elapsedTime;
+    process* executingProcess;
+    queue* highPriorityQueue;
+    queue* lowPriorityQueue;
+    queue* IOPriorityQueue;
+
+} cpu_t;
 
 //assinatura das funções
 const char* getPriorityAsString(int);
@@ -45,11 +65,18 @@ const char* getIoTypeAsString(int);
 void printProcess(process*);
 pid_t generateRandomPID();
 pid_t generateIncrementalPID();
-int generateRandomTime();
+int generateRandomDuration();
+int generateRandomStartingTime();
 int generateRandomPriority();
 int generateRandomIO();
-process* initRandomProcess(pid_t);
+process* initRandomProcess();
 process** initRandomProcesses();
+int allProcessFinished(process**);
+void insertIntoQueue(queue*,process*);
+process* nextFromQueue(queue*);
+void addProcessToQueueAfterIO(cpu_t*, process*);
+void addProcessToQueue(cpu_t*, process*);
+void addReadyProcessesToCPU(cpu_t*, process**);
 
 const char* getPriorityAsString(int priority) {
     switch(priority){
@@ -61,7 +88,8 @@ const char* getPriorityAsString(int priority) {
 }
 
 const char* getStatusAsString(int status) {
-    switch(status){
+    switch(status) {
+        case NOT_STARTED: return "Not started";
         case READY: return "Ready";
         case RUNNING: return "Running";
         case BLOCKED: return "Blocked";
@@ -82,25 +110,32 @@ const char* getIoTypeAsString(int io_type) {
 }
 
 void printProcess(process* p) {
-    printf("PID : %d\nDuration : %d\nPPID : %d\nPRIORITY : %s\nSTATUS : %s\nIO TYPE : %s\n\n",
-    p->pid,
-    p->duration,
-    p->ppid,
-    getPriorityAsString(p->priority),
-    getStatusAsString(p->status),
-    getIoTypeAsString(p->ioType));
+    printf("=== Process Description ===\n");
+    printf("PID: %d\nDuration: %d\nPPID: %d\nPRIORITY: %s\nSTATUS: %s\nIO TYPE: %s\n\n",
+        p->pid,
+        p->duration,
+        p->ppid,
+        getPriorityAsString(p->priority),
+        getStatusAsString(p->status),
+        getIoTypeAsString(p->ioType));
 }
 
-pid_t generateRandomPID() {
-    return rand() % 1000;
-}
-
+// generate PID incrementally
 pid_t generateIncrementalPID() {
     static pid_t lastPID = 0;
     return ++lastPID;
 }
 
-int generateRandomTime() {
+// Randomness generators
+pid_t generateRandomPID() {
+    return rand() % 1000;
+}
+
+int generateRandomDuration() {
+    return (rand() % MAX_DURATION) + 1;
+}
+
+int generateRandomStartingTime() {
     return (rand() % MAX_STARTING_TIME) + 1;
 }
 
@@ -112,13 +147,15 @@ int generateRandomIO() {
     return rand() % IO_TYPE_SIZE;
 }
 
-process* initRandomProcess(pid_t pid) {
+// Process initializers
+process* initRandomProcess() {
     process* proc = malloc(sizeof(process));
-    proc->duration = 0;
-    proc->pid = pid;
-    proc->ppid = 0;     // fix parent pid at 0, TODO change?
+    proc->duration = generateRandomDuration();
+    proc->startingTime = generateRandomStartingTime();
+    proc->pid = generateIncrementalPID();
+    proc->ppid = 0;     // fix parent pid at 0, TODO: change?
     proc->priority = generateRandomPriority();
-    proc->status = READY;
+    proc->status = NOT_STARTED;
     proc->ioType = generateRandomIO();
     return proc;
 }
@@ -126,11 +163,27 @@ process* initRandomProcess(pid_t pid) {
 process** initRandomProcesses() {
     // Allocate processes in the heap
     process** initializedProcesses = calloc(MAX_PROCESSES, sizeof(process*));
-    for (pid_t pid = generateIncrementalPID(); pid < MAX_PROCESSES; pid = generateIncrementalPID()) {
-        initializedProcesses[pid] = initRandomProcess(pid);
-        printProcess(initializedProcesses[pid]);
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        initializedProcesses[i] = initRandomProcess();
+        printProcess(initializedProcesses[i]);
     }
     return initializedProcesses;
+}
+
+queue* initQueue() {
+    queue* q = malloc(sizeof(queue));
+    q->processes = calloc(MAX_PROCESSES, sizeof(process*));
+    q->size = 0;
+    return q;
+}
+
+cpu_t* initCPU() {
+    cpu_t* cpu = malloc(sizeof(cpu_t));
+    cpu->highPriorityQueue = initQueue();
+    cpu->lowPriorityQueue = initQueue();
+    cpu->IOPriorityQueue = initQueue();
+    cpu->cycles = 0;
+    return cpu;
 }
 
 int allProcessFinished(process** processes) {
@@ -141,19 +194,70 @@ int allProcessFinished(process** processes) {
     return TRUE;
 }
 
+void insertIntoQueue(queue* q, process* p) {
+    q->processes[q->size] = p;
+    q->size++;
+}
+
+process* nextFromQueue(queue* q) {
+    process* p = q->processes[0];
+    for (int i = 0; i < q->size - 1; ++i) {
+        q->processes[i] = q->processes[i+1];
+    }
+    return p;
+}
+
+void insertIntoQueueAfterIO(cpu_t* cpu, process* p) {
+    switch(p->ioType) {
+        case DISK:
+            insertIntoQueue(cpu->lowPriorityQueue, p);
+            break;
+        case MAGNETIC_TAPE:
+        case PRINTER:
+            insertIntoQueue(cpu->highPriorityQueue, p);
+        case NONE:
+            break;
+    }
+}
+
+void addProcessToQueue(cpu_t* cpu, process* p) {
+    if(p->returnedFromIO) {
+        insertIntoQueueAfterIO(cpu, p);
+        p->returnedFromIO = FALSE;
+    }
+    else if(p->preempted) {
+        insertIntoQueue(cpu->lowPriorityQueue, p);
+        p->preempted = FALSE;
+    }
+}
+
+void addReadyProcessesToCPU(cpu_t* cpu, process** processes) {
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        // check if process has reached its starting time
+        if (processes[i]->startingTime == cpu->cycles) {
+            printf("Adicionando processo %d a fila de processos prontos com alta prioridade\n",
+                processes[i]->pid);
+            processes[i]->status = READY;
+            insertIntoQueue(cpu->highPriorityQueue, processes[i]);
+        } else if(processes[i]->status == READY)
+            addProcessToQueue(cpu, processes[i]);
+    }
+}
+
 int main() {
 
     srand(time(0));
 
-    queue* highPriorityQueue;
-    queue* lowPriorityQueue;
-    queue* IOPriorityQueue;
-
+    cpu_t* cpu = initCPU();
     process** processes = initRandomProcesses();
 
-    // while(!allProcessFinished(processes)) {
-    //     // do something with the processes
-    // }
+    while(!allProcessFinished(processes)) {
+        printf(ANSI_COLOR_BLUE "Ciclo %d\n" ANSI_COLOR_RESET, cpu->cycles++);
+
+        addReadyProcessesToCPU(cpu, processes);
+
+        // handle ready processes
+    }
 
     // int amount;
     // printf("\nInsira a quantidade de processos que deseja criar : ");
@@ -163,8 +267,8 @@ int main() {
     // printf("Insira o tamanho do quantum (time slice) : ");
     // scanf("%d",&timeSlice);
 
-    printf("%d\n",generateRandomPID());
-    printf("%d\n",generateRandomTime());
+    // printf("%d\n",generateRandomPID());
+    // printf("%d\n",generateRandomTime());
 
     return 0;
 }
